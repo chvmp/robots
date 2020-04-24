dab483109d2836cfe3e35e1c08f61893df8e884c
Juan Miguel Jimeno <jimenojmm@gmail.com>
2020-04-24 22:52:51 +0800

    added dream_walker

---
+++ dream_walker_config/include/gait_config.h
@@ -0,0 +1,14 @@
+#ifndef GAIT_CONFIG_H
+#define GAIT_CONFIG_H
+
+#define KNEE_ORIENTATION        ">>"
+#define PANTOGRAPH_LEG          false
+#define MAX_LINEAR_VELOCITY_X   0.5
+#define MAX_LINEAR_VELOCITY_Y   0.25
+#define MAX_ANGULAR_VELOCITY_Z  1.0
+#define SWING_HEIGHT            0.1
+#define STANCE_DEPTH            0.0
+#define STANCE_DURATION         0.25
+#define NOMINAL_HEIGHT          0.5
+
+#endif
