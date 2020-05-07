3e3c8d6ef2966a08b220f123b4d1706df9a74c08
Juan Miguel Jimeno <jimenojmm@gmail.com>
2020-05-07 21:07:00 +0800

    fixed walking param

--- littledog_config/include/gait_config.h
+++ littledog_config/include/gait_config.h
@@ -6,9 +6,9 @@
 #define MAX_LINEAR_VELOCITY_X   0.5
 #define MAX_LINEAR_VELOCITY_Y   0.25
 #define MAX_ANGULAR_VELOCITY_Z  1.0
-#define SWING_HEIGHT            0.04
+#define SWING_HEIGHT            0.02
 #define STANCE_DEPTH            0.0
-#define STANCE_DURATION         0.25
-#define NOMINAL_HEIGHT          0.15
+#define STANCE_DURATION         0.2
+#define NOMINAL_HEIGHT          0.14
 
 #endif
